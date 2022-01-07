a9944d656cc257d04c13ce530bee8e6e57fb15d9
xkino <xkinoedik@gmail.com>
2022-01-07 16:14:52 +0300

    hello world

---
+++ src/main/java/com/springdemo/helloworld/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.springdemo.helloworld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping
+    public String helloWorld(){
+        return "Hello World from Spring Boot";
+    }
+
+    @RequestMapping("/goodbye")
+    public String goodbye(){
+        return "Goodbye from Spring Boot";
+    }
+}
